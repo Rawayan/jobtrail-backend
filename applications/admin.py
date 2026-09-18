7cb82e60c5aff63afd19ace0297e975234f0c213
Rawayan <rawfir.ayan@gmail.com>
2026-09-19 02:27:31 +0600

    Add Application model and admin

--- applications/admin.py
+++ applications/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Application
+
+
+@admin.register(Application)
+class ApplicationAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "company",
+        "position",
+        "owner",
+        "status",
+        "job_type",
+        "applied_on",
+        "expected_salary",
+        "created_at",
+    )
+
+    list_filter = (
+        "status",
+        "job_type",
+    )
+
+    search_fields = (
+        "company",
+        "position",
+        "owner__username",
+    )
